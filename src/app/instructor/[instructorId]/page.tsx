86d5c14f926d3731add073b45df8c7b0462695b8
Sultan <sultan250t@gmail.com>
2025-09-19 15:31:19 +0500

    finale

--- src/app/instructor/[instructorId]/page.tsx
+++ src/app/instructor/[instructorId]/page.tsx
@@ -1,13 +1,36 @@
 import InstructorProfileHeader from "@/app/components/instructor-profile/InstructorProfileHeader";
 import InstructorProfileContent from "@/app/components/instructor-profile/InstructorProfileContent";
-
-export default function InstructorProfilePage() {
+interface InstructorPageProps {
+  params: {
+    instructorId: string; // Параметр из URL, например, /instructor/123
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+export default function InstructorPage({ params }: InstructorPageProps) {
   return (
     <>
       <InstructorProfileHeader />
       <InstructorProfileContent />
     </>
   );
-}
+
+
 
 // OneDrive\Рабочий стол\etutordiplom\src\app\instructor\[instructorId]\page.tsx
+// yes
+interface InstructorPageProps {
+  params: {
+    instructorId: string; // Параметр из URL, например, /instructor/123
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+  const { instructorId } = params; 
+  return (
+    <div>
+      <h1>Профиль инструктора: {instructorId}</h1>
+      const instructorData = await getInstructor(instructorId)
+      <p>Это страница профиля инструктора с ID: {instructorId}</p>
+    </div>
+  );
+}
